6bda0b379319bc8ea5dc1fabe7afd43e76f69289
runhuasun <127193621+runhuasun@users.noreply.github.com>
2023-04-06 01:08:19 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 import preact from '@astrojs/preact'
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'server',
+  adapter: vercel(),
 })
+
